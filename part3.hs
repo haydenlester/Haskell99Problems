95ea5cd0c4c5327bcbb8ad5b77949f4ab69eb9bd
Hayden Lester <haydenlester99@protonmail.com>
2024-03-20 07:37:14 -0500

    refac: consistency

--- part3.hs
+++ part3.hs
@@ -1,10 +1,12 @@
 import System.Random
 
 -- 21
+-- Insert an element at a given position into a list.
 insertAt :: a -> [a] -> Int -> [a]
-insertAt x arr i = take (i-1) arr ++ (x : drop (i-1) arr)
+insertAt x xs i = take (i-1) xs ++ (x : drop (i-1) xs)
 
 -- 22
+-- Create a list containing all integers within a given range.
 range :: Int -> Int -> [Int]
 range l r = [l..r]
 
@@ -14,17 +16,21 @@ removeAt i xs = take i xs ++ drop (i+1) xs
 
 -- random index
 rndI :: RandomGen g => [a] -> g -> (Int, g)
-rndI = randomR . (\x -> (0, (length x)-1))
+rndI xs = randomR (0, (length xs)-1)
 
--- 23 full recursive
+-- 23
+-- Extract a given number of randomly selected elements from a list.
 rndSelect :: RandomGen g => [a] -> Int -> g -> ([a], g)
 rndSelect [] _ gen = ([], gen)
 rndSelect _ 0 gen  = ([], gen)
-rndSelect l c gen
-  | c == (length l)   = (l, gen)
-  | otherwise         = rndSelect (removeAt k l) c gen'
-                        where (k, gen') = rndI l gen
+rndSelect xs c gen
+  | c == (length xs)  = (xs, gen)
+  | otherwise         = rndSelect (removeAt i xs) c gen'
+                        where (i, gen') = rndI xs gen
 
--- 23
 rndSelectIO :: [a] -> Int -> IO [a]
-rndSelectIO l c = getStdRandom $ rndSelect l c
+rndSelectIO xs c = getStdRandom $ rndSelect xs c
+
+-- 24
+-- Draw N different random numbers from the set 1..M.
+
