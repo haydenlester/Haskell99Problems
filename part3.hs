d79819f327a066de81738a864d0ab98477a2f2d5
Hayden Lester <haydenlester99@protonmail.com>
2024-03-21 07:06:15 -0500

    refac: fix 23

--- part3.hs
+++ part3.hs
@@ -14,24 +14,17 @@ range l r = [l..r]
 removeAt :: Int -> [a] -> [a]
 removeAt i xs = take i xs ++ drop (i+1) xs
 
--- random index
-rndI :: RandomGen g => [a] -> g -> (Int, g)
-rndI xs = randomR (0, (length xs)-1)
-
 -- 23
 -- Extract a given number of randomly selected elements from a list.
-rndSelect :: RandomGen g => [a] -> Int -> g -> ([a], g)
-rndSelect [] _ gen = ([], gen)
-rndSelect _ 0 gen  = ([], gen)
-rndSelect xs c gen
-  | c == (length xs)  = (xs, gen)
-  | otherwise         = rndSelect (removeAt i xs) c gen'
-                        where (i, gen') = rndI xs gen
-
-rndSelectIO :: [a] -> Int -> IO [a]
-rndSelectIO xs c = getStdRandom $ rndSelect xs c
+rndSelect :: [a] -> Int -> IO [a]
+rndSelect [] _ = return []
+rndSelect _ 0 = return []
+rndSelect xs c = do 
+    i <- randomRIO (0, (length xs)-1)
+    rest <- rndSelect (removeAt i xs) (c-1)
+    return $ (xs!!i) : rest
 
 -- 24
 -- Draw N different random numbers from the set 1..M.
 diffSelect :: Int -> Int -> IO [Int]
-diffSelect n m = rndSelectIO [1..m] n
+diffSelect n m = rndSelect [1..m] n
