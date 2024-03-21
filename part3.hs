37dd133e8a4faf61bba9c27b4014675678e24a53
Hayden Lester <haydenlester99@protonmail.com>
2024-03-21 09:19:46 -0500

    prob 26

--- part3.hs
+++ part3.hs
@@ -33,3 +33,10 @@ diffSelect n m = rndSelect [1..m] n
 -- Generate a random permutation of the elements of a list.
 rndPermu :: [a] -> IO [a]
 rndPermu xs = rndSelect xs $ length xs
+
+-- 26
+-- Generate combinations of K distinct objects chosen from the N elements of a list.
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [[]]
+combinations _ [] = []
+combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
