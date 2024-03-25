7324353cf48d42feff0b3bf540683f8bd10fb7ed
Hayden Lester <haydenlester99@protonmail.com>
2024-03-25 13:04:29 -0500

    prob 31

--- part4.hs
+++ part4.hs
@@ -7,9 +7,16 @@ isPrime :: Int -> Bool
 isPrime n = [] == [p | p <- takeWhile ((<= n) . (^2)) sixK, n `mod` p == 0]
   where sixK  = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
 
--- more readable equivalent version
+-- slightly more readable equivalent version
 -- see wikipedia on primality testing
 isPrime' :: Int -> Bool
-isPrime' n = [] == takeWhile (\p -> p*p <= n) valids
-  where sixK    = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
+isPrime' n = [] == takeWhile ltEqSqrt valids
+  where ltEqSqrt p = p*p <= n
+        sixK    = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
         valids  = [p | p <- sixK, n `mod` p == 0]
+
+-- 32
+-- Use Euclid's algorithm to determine GCD of two integers.
+gcd' :: Int -> Int -> Int
+gcd a b
+  |
