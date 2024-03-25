905ac0608912af2f51caa2d970044cce90fcbca9
Hayden Lester <haydenlester99@protonmail.com>
2024-03-25 13:00:33 -0500

    prob 31

--- part4.hs
+++ part4.hs
@@ -9,7 +9,7 @@ isPrime n = [] == [p | p <- takeWhile ((<= n) . (^2)) sixK, n `mod` p == 0]
 
 -- more readable equivalent version
 -- see wikipedia on primality testing
-isPrime :: Int -> Bool
-isPrime n = [] == takeWhile (\p -> p*p <= n) valids
+isPrime' :: Int -> Bool
+isPrime' n = [] == takeWhile (\p -> p*p <= n) valids
   where sixK    = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
         valids  = [p | p <- sixK, n `mod` p == 0]
