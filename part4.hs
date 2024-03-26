0ca77d2fd5dd1ebd8bacab8aeeeae6c09cd6c5ea
Hayden Lester <haydenlester99@protonmail.com>
2024-03-26 10:37:18 -0500

    refac: elegance

--- part4.hs
+++ part4.hs
@@ -1,21 +1,17 @@
 import Data.Function (on)
+import Data.List (intersect)
+
+-- delegate for 31 and 35
+primeFs :: Int -> [Int]
+primeFs n = [p | p <- takeWhile ((<= n) . (^2)) sixK, n `mod` p == 0]
+  where sixK  = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
 
 -- 31
 -- Determine whether a given integer is prime
 -- Using 6k +/- 1 optimization
--- This impl is mostly just an experiment in list comprehensions
--- It is not readable.
+-- (i know its hard to read)
 isPrime :: Int -> Bool
-isPrime n = [] == [p | p <- takeWhile ((<= n) . (^2)) sixK, n `mod` p == 0]
-  where sixK  = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
-
--- slightly more readable equivalent version
--- see wikipedia on primality testing
-isPrime' :: Int -> Bool
-isPrime' n = [] == takeWhile ltEqSqrt valids
-  where ltEqSqrt p = p*p <= n
-        sixK    = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
-        valids  = [p | p <- sixK, n `mod` p == 0]
+isPrime n = [] == primeFs n
 
 -- 32
 -- Use Euclid's algorithm to determine GCD of two integers.
@@ -28,7 +24,7 @@ gcd' a b = case compare a b of
 -- 33
 -- Determine whether two positive integers are coprime.
 coprime :: Int -> Int -> Bool
-coprime x y = gcd x y == 1
+coprime x y = [] == intersect (primeFs x) (primeFs y)
 
 -- 34
 -- Calculate Euler's totient function
