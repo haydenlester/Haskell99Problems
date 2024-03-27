84fb49d87943f36a42b09ef5ac289b34643223cf
Hayden Lester <haydenlester99@protonmail.com>
2024-03-27 03:25:57 -0500

    prob 37: formulaic totient

--- part4.hs
+++ part4.hs
@@ -1,18 +1,25 @@
 import Data.Function (on)
 import Data.List (group, intersect)
 
+-- Delegate 
+-- 6k +/- 1 optimization for primality tests
+sixK :: [Int]
+sixK  = (2:3:[x + i | x <- [6,12..], i <- [-1,1]])
+
 -- Delegate for 31 and 35
--- Lists all prime factors for n, except for
---  n itself when n is prime.
+-- Lists all prime factors for n, except n itself
 primeFactors :: Int -> [Int]
-primeFactors n = [p | p <- takeWhile ((<= n) . (^2)) sixK, n `mod` p == 0]
-  where sixK  = [2, 3, 5] ++ [6*k + i | k <- [1..], i <- [1, 5]]
+primeFactors n = filter divisGate $ takeWhile sizeGate sixK
+  where divisGate x = (n `mod` x == 0)
+        sizeGate  x = (x^2 <= n)
 
 -- 31
 -- Determine whether a given integer is prime
--- Using 6k +/- 1 optimization
 isPrime :: Int -> Bool
-isPrime n = [] == primeFactors n
+isPrime = null . primeFactors
+
+-- Delegate sequence of all primes
+primes = [p | p <- sixK, isPrime p]
 
 -- 32
 -- Use Euclid's algorithm to determine GCD of two integers.
@@ -25,13 +32,13 @@ gcd' a b = case compare a b of
 -- 33
 -- Determine whether two positive integers are coprime.
 coprime :: Int -> Int -> Bool
-coprime x y = [] == intersect (primeFactors x) (primeFactors y)
+coprime x y = 1 == gcd x y
 
 -- 34
 -- Calculate Euler's totient function
 totient :: Int -> Int
 totient 1 = 1
-totient m = length [x | x <- [1..m-1], coprime x m]
+totient m = length [x | x <- [1..m-1], (coprime x m)]
 
 -- 35
 -- Determine the composed prime factors of n
@@ -46,3 +53,12 @@ composePrimeFactors n = case primeFactors n of
 primeFactorsMult :: Int -> [(Int, Int)]
 primeFactorsMult = map toMult . group . composePrimeFactors
   where toMult xs = (head xs, length xs)
+
+-- 37
+-- Calculate Euler's totient function phi(m)
+-- Use improved formulaic method
+phi :: Int -> Int
+phi n = phi' $ primeFactorsMult n
+phi' :: [(Int, Int)] -> Int
+phi' []           = 1
+phi' ((p, m):ns)  = (p - 1) * p ^ (m - 1) * (phi' ns)
